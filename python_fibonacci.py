a730b0f026e3da49f140ee4e544bca7e496986a8
AkshayHasabe <ahasabe5@gmail.com>
2022-07-09 23:23:09 +0530

    modjen

--- python_fibonacci.py
+++ python_fibonacci.py
@@ -1,4 +1,19 @@
+# This code prints fibinacci series of first 10
 n=10
+n1=0
+n2=1
+print("Fibonacci Series of first",n,"number is:", n1, n2, end=" ")
+for i in range(2,n):
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print(n3, end=" ")
+
+print("\n")
+
+
+# This code prints fibinacci series of first 15
+n=15
 n1=0
 n2=1
 print("Fibonacci Series of first",n,"number is:", n1, n2, end=" ")
